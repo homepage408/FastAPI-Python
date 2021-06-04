e8c2df7db2efd88ec751a2ed88a91408d5fa1ee3
homepage408 <homepage408@gmail.com>
2021-06-04 07:11:13 +0700

    request body

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get('/')
 def index():
-    return 'HEy'
+    return {'data': 'blog list'}
 
 
-@app.get('/about')
-def about():
-    return {'page': 'about'}
+@app.get('/blog')
+def blog(limit=10, published: bool = True, sort: Optional[str] = None):
+
+    if published:
+        return {"data": f"{limit} published blog from the database"}
+    else:
+        return {'data': f'{limit} blog from the database'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data blog': 'unpublished'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data blog': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    pubslished_at: Optional[bool]
+
+
+@app.post('/blog')
+def createBlog(request: Blog):
+    return {'msg': f'Blog has created with title as {request.title}'}
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="127.0.0.1", port='9000')
