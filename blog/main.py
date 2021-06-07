681eff37e8e5b0fc45703281b63504c3ead7c098
Teguh Setiawan <homepage408@gmail.com>
2021-06-07 10:26:34 +0700

    database connections

---
+++ blog/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from . import schemas, models
+from .database import engine
+
+
+app = FastAPI()
+
+
+
+models.Base.metadata.create_all(engine)
+
+@app.post('/blog')
+def blog_request(request: schemas.BlogSchemas):
+    return request
---
+++ blog/schemas.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+class BlogSchemas(BaseModel):
+    title: str
+    body:str
